17678e6b2ba2ab190af6174b5a5ebb337195014a
XthomasB <jt.xbowen@gmail.com>
2016-08-12 20:26:26 -0400

    second commit

---
+++ Scripts/apps.js
@@ -0,0 +1,13 @@
+/*Author: Hassan Kobeissi
+Date Created: 2016-08-09
+Date Modified: 2016-08-09
+Version: 0.0.1
+Website: 
+FileDescription: JavaScript Styling for the Index.html page
+*/ 
+/*On  click Function - retreived  from StackOverFlow*/ 
+$(".toggle").on("click", function () {
+
+    $(".container").toggleClass("microsoft");
+
+});
